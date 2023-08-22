8e7486cbd5540d539c625f1cd6d21e5d29ef975d
wlals1126 <103996321+wlals1126@users.noreply.github.com>
2023-08-22 17:52:51 +0900

    :bug: :: babel 파싱 에러 해결

--- min-log/next.config.js
+++ min-log/next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { widthContentlayer } = require("next-contentlayer");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false,
+};
+
+module.exports = widthContentlayer(options);

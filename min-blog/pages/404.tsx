5ce4edc64ec5fd7bfb531c06d7891f923283ecf0
wlals1126 <103996321+wlals1126@users.noreply.github.com>
2023-08-28 22:04:32 +0900

    :sparkles: :: 404페이지 추가

---
+++ min-blog/pages/404.tsx
@@ -0,0 +1,11 @@
+import { ErrorContainer } from 'styles/Main';
+
+const NotFound = () => {
+	return (
+		<ErrorContainer>
+			<p>페이지를 찾을 수 없습니다</p>
+		</ErrorContainer>
+	);
+};
+
+export default NotFound;

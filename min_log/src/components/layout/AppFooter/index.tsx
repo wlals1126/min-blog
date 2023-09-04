84ffe36d8fc37707b8e74a50603fa296a49e4ceb
wlals1126 <103996321+wlals1126@users.noreply.github.com>
2023-09-04 20:06:27 +0900

    :zap: :: Link href 수정

--- min_log/src/components/layout/AppFooter/index.tsx
+++ min_log/src/components/layout/AppFooter/index.tsx
@@ -1,33 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link';
-import React from 'react';
-import * as a from './style';
+import Link from "next/link";
+import React from "react";
+import * as a from "./style";
 
 const AppFooter = () => {
-	return (
-		<a.AppFooterBox>
-			<div>
-				<a href="https://github.com/wlals1126" target="_blank">
-					<img src="/github.svg" alt="github_icon" />
-					MinLog
-				</a>
-				<a href="" target="_blank">
-					<img src="" alt="about_icon" />
-				</a>
-				<div>
-					<div>
-						<img src="/email.jpg" alt="email_icon" />
-					</div>
-					zzzm1n126@gmail.com
-				</div>
-			</div>
-			<Link href="/test" legacyBehavior>
-				<a>
-					<div />
-				</a>
-			</Link>
-		</a.AppFooterBox>
-	);
+  return (
+    <a.AppFooterBox>
+      <div>
+        <a href="https://github.com/wlals1126" target="_blank">
+          <img src="/github.svg" alt="github_icon" />
+          MinLog
+        </a>
+        <a href="" target="_blank">
+          <img src="" alt="about_icon" />
+        </a>
+        <div>
+          <div>
+            <img src="/email.jpg" alt="email_icon" />
+          </div>
+          zzzm1n126@gmail.com
+        </div>
+      </div>
+      <Link href="/login" legacyBehavior>
+        <a>
+          <div />
+        </a>
+      </Link>
+    </a.AppFooterBox>
+  );
 };
 
 export default AppFooter;

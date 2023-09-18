949179c909eaa35d43a9e49d49f6993782c6e7a4
wlals1126 <103996321+wlals1126@users.noreply.github.com>
2023-09-18 15:02:00 +0900

    :sparkles: :: sagas 폴더 생성

---
+++ min_log/src/sagas/posts.tsx
@@ -0,0 +1,25 @@
+import { loadCategoriesAsync, loadPostsAsync } from "@/reducers/posts";
+import axios from "axios";
+import { call, all, fork, takeLatest, put } from "redux-saga/effects";
+
+async function loadAllPostsAPI(query: any) {
+  return await axios.get(`/post`);
+}
+
+function* loadAllPosts(action: ReturnType<typeof loadPostsAsync.request>) {
+  try {
+    const result = yield call(loadAllPostsAPI, action.payload);
+    yield put(loadPostsAsync.success(result.data));
+  } catch (error: any) {
+    console.error(error);
+    yield put(loadPostsAsync.failure(error));
+  }
+}
+
+function* watchLoadAllPosts() {
+  yield takeLatest(loadPostsAsync.request, loadAllPosts);
+}
+
+export default function* postsSaga() {
+  yield all([fork(watchLoadAllPosts)]);
+}

afa60a5f21b1fa81e069aa020d5a9d525d6584ba
wlals1126 <103996321+wlals1126@users.noreply.github.com>
2023-09-19 15:23:11 +0900

    :sparkles: :: user saga 생성

---
+++ min_log/src/sagas/user.tsx
@@ -0,0 +1,62 @@
+import { loadingEnd, loadingStart } from "@/reducers/loading";
+import {
+  LOAD_USER_REQUEST,
+  LOGIN_REQUEST,
+  loadUserAsync,
+  loginAsync,
+} from "@/reducers/user";
+import axios, { AxiosResponse } from "axios";
+import {
+  call,
+  fork,
+  put,
+  takeEvery,
+  takeLatest,
+  all,
+} from "redux-saga/effects";
+
+function loadUserAPI() {
+  return axios.get(`/user`);
+}
+
+function* loadUser() {
+  try {
+    const result: AxiosResponse<any> = yield call(loadUserAPI);
+    yield put(loadUserAsync.success(result.data));
+  } catch (error: any) {
+    console.log(error);
+    yield put(loadUserAsync.failure(error));
+  }
+}
+
+function* watchLoadUser() {
+  yield takeEvery(LOAD_USER_REQUEST, loadUser);
+}
+
+async function loginAPI(loginData: any) {
+  return axios.post(`/user`, loginData, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+}
+
+function* login(action: ReturnType<typeof loginAsync.request>) {
+  yield put(loadingStart(action.type));
+  try {
+    const result: AxiosResponse<any> = yield call(loginAPI, action.payload);
+    yield put(loginAsync.success(result.data));
+  } catch (error: any) {
+    console.log(error);
+    yield put(loginAsync.failure(error));
+  }
+  yield put(loadingEnd(action.type));
+}
+
+function* watchLogin() {
+  yield takeLatest(LOGIN_REQUEST, login);
+}
+
+export default function* userSaga() {
+  yield all([fork(watchLogin), fork(watchLoadUser)]);
+}
